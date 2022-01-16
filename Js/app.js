7288fcf38cac41ecc580ea5d79a7f533d82f3015
RL2020 <loc.fitness1@gmail.com>
2022-01-16 11:39:58 -0500

    started class Me

---
+++ Js/app.js
@@ -0,0 +1,9 @@
+class Me {
+
+    constructor(name, projects, about, resume) {
+        this.name = name;
+        this.projects = projects;
+        this.about = about;
+        this.resume = resume;
+    }
+}
