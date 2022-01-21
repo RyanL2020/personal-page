7f133154332cdd057bce536fe6ff60f26e45428e
RL2020 <loc.fitness1@gmail.com>
2022-01-20 19:51:25 -0500

    added index.js, changed app.js too me.js

--- Js/me.js
+++ Js/me.js
@@ -1,9 +1,12 @@
 class Me {
 
+    static = []
     constructor(name, projects, about, resume) {
         this.name = name;
         this.projects = projects;
         this.about = about;
         this.resume = resume;
+
+        Me.all.push(this)
     }
 }
